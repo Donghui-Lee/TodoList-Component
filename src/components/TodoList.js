f9105f136288287fe63945e8780ede9397308769
Donghui-Lee <ehdgml3206@naver.com>
2020-01-29 01:23:14 +0900

    기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,14 +11,16 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    const state = useTodoState();
-    console.log(state);
+    const todos = useTodoState();
+
     return (
         <TodoListBlock>
-            <TodoItem text="프로젝트 생성하기" done={true}></TodoItem>
-            <TodoItem text="컴포넌트 스타일링하기" done={true}></TodoItem>
-            <TodoItem text="Context 만들기" done={false}></TodoItem>
-            <TodoItem text="기능 구현하기" done={false}></TodoItem>
+            {todos.map(todo => (
+                <TodoItem key={todo.id}
+                          id={todo.id}
+                          text={todo.text}
+                          done={todo.done} />
+            ))}
         </TodoListBlock>
     );
 }
